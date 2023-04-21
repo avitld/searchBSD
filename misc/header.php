6ec195b8b783efe296828db72ed900e787e0bf76
BSDFemboy <98060621+BSDFemboy@users.noreply.github.com>
2023-04-21 21:06:36 +0300

    forgot / oopsies

--- misc/header.php
+++ misc/header.php
@@ -7,6 +7,6 @@
 		<title>SearchBSD</title>
 		>meta name ="description" content="The BSD Search Engine." />
 		<meta name="referrer" content="no-referrer" />
-		<link rel="icon" type="image/x-icon" href="img/favicon.png">
+		<link rel="icon" type="image/x-icon" href="img/favicon.png" />
 		<link rel="stylesheet" type="text/css" href="static/css/styles.css" />
 	</head>
