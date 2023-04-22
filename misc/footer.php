c941c2ba1db4cddb1eeb0ee3d360994d69bac003
BSDFemboy <98060621+BSDFemboy@users.noreply.github.com>
2023-04-22 16:33:18 +0300

    Delete misc directory

--- misc/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="footer">
-	<a href="https://github.com/BSDFemboy/searchbsd/" target="_blank">SearchBSD, The BSD Search and Troubleshoot engine</a>
-	<a href="./config.php">Configuration</a>
-	<a href="./support.php">Support Us</a>
-</div>
-</body>
-</html>
