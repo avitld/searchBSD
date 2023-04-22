3a75782ff6019460d01e86785b71b2d7be0f1121
BSDFemboy <98060621+BSDFemboy@users.noreply.github.com>
2023-04-22 19:26:06 +0300

    Update support.php

---
+++ support.php
@@ -0,0 +1,14 @@
+<?php require "misc/header.php"; ?>
+
+	<body>
+        <div class="settings">
+            <h1>Support The Project</h1>
+            <hr class="smhr" />
+            <p>Donate to Avityred:</p>
+            <a href="https://liberapay.com/avityred/donate" style="color: cyan;">Liberapay</a>
+            <br/>
+            <hr class="smhr" />
+            <a href="index.php" style="color: white; text-decoration: none;">Go Home</a>
+        </div>
+
+<?php require "misc/footer.php"; ?>
