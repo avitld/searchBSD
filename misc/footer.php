c5905ff68ea1eb7ac8d4e3ee7c5acbe0b5364400
BSDFemboy <98060621+BSDFemboy@users.noreply.github.com>
2023-04-22 20:37:14 +0300

    Update footer.php

--- misc/footer.php
+++ misc/footer.php
@@ -1,6 +1,5 @@
 <div class="footer">
 	<a href="https://github.com/BSDFemboy/searchbsd/" target="_blank">Source</a>
-	<a href="./config.php">Configuration</a>
 	<a href="./support.php">Support</a>
 </div>
 </body>
